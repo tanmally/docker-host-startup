faf965b5213cd9572318a0764c94342a60874dd5
Thanneer <tm@beyondbytes.co.in>
2014-01-21 22:10:49 +0530

    added echo stms

--- docker-init.sh
+++ docker-init.sh
@@ -19,16 +19,27 @@ docker rm $(docker ps -a -q)
 if [ ! -z "$domain" ]
 then
 docker run -d -p  172.30.1.1:53:53/udp -name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain $domain
-docker run -i -v /var/run/docker.sock:/docker.sock -name skydock -link skydns:skydns crosbymichael/skydock -ttl 30 -environment prod -s /docker.sock -domain $domain
+docker run -d -v /var/run/docker.sock:/docker.sock -name skydock -link skydns:skydns crosbymichael/skydock -ttl 30 -environment docker -s /docker.sock -domain $domain
+domainname="<service>.$domain"
 echo "Docker service dicovery domain : *.$domain " 
 else
 docker run -d -p 172.30.1.1:53:53/udp -name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain docker.bbytes.com
-docker run -i -v /var/run/docker.sock:/docker.sock -name skydock -link skydns:skydns crosbymichael/skydock -ttl 30 -environment prod -s /docker.sock -domain docker.bbytes.com
+docker run -d -v /var/run/docker.sock:/docker.sock -name skydock -link skydns:skydns crosbymichael/skydock -ttl 30 -environment docker -s /docker.sock -domain bbytes.com
+domainname="<service>.docker.bbytes.com"
 echo "Docker service dicovery domain : *.docker.bbytes.com " 
 fi
 
 
 echo "Docker dns and service dicovery service started "
+
+echo 'nameserver 172.30.1.1' >> /etc/resolvconf/resolv.conf.d/head
+sudo ifdown --exclude=lo -a && sudo ifup --exclude=lo -a
+echo 'Docker host machine dns name server is updated to dicover service based on domain address'
+echo "try pinging the $domainname"
+echo "the value of <service> is the second part of image name for eg: it is 'tomcat' for image with name 'bbytes/tomcat'"
+
+
+
 
 
 
