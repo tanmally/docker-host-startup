c8200bd51417bd39e1e75395546d96cee433c894
Thanneer <tm@beyondbytes.co.in>
2014-01-21 16:38:00 +0530

    Added dns and service discovery service

--- docker-init.sh
+++ docker-init.sh
@@ -9,3 +9,26 @@ cp -f docker.conf /etc/init/
 echo "Docker container dns 172.30.1.1"
 echo "Starting Docker"
 service docker start
+echo "Waiting for Docker to start.."
+sleep 5s
+echo "Docker started.."
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+if [ ! -z "$domain" ]
+then
+docker run -d -p  172.30.1.1:53:53/udp -name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain $domain
+docker run -i -v /var/run/docker.sock:/docker.sock -name skydock -link skydns:skydns crosbymichael/skydock -ttl 30 -environment prod -s /docker.sock -domain $domain
+echo "Docker service dicovery domain : *.$domain " 
+else
+docker run -d -p 172.30.1.1:53:53/udp -name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain docker.bbytes.com
+docker run -i -v /var/run/docker.sock:/docker.sock -name skydock -link skydns:skydns crosbymichael/skydock -ttl 30 -environment prod -s /docker.sock -domain docker.bbytes.com
+echo "Docker service dicovery domain : *.docker.bbytes.com " 
+fi
+
+
+echo "Docker dns and service dicovery service started "
+
+
+
